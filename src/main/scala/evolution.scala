3119168b706e59bf65b23de6fd50624f36366ecc
Alatreon42 <60519223+Alatreon42@users.noreply.github.com>
2020-04-10 15:32:32 +0200

    Update de l'arbre des upgrades
    La branche Tank -> Sniper -> Stalker devrait avoir Assassin en troisième puis Stalker, mais la ressource est introuvable :(

--- src/main/scala/evolution.scala
+++ src/main/scala/evolution.scala
@@ -1,12 +1,12 @@
 object Evolution
 {
 	var evolutions = Map(
-				"tank" -> Some((("twin",100),("sniper",100),("tripleshot",100),("",100))),
-				"twin" -> Some((("triplet",100),("quadtank",100),("",100),("",100))),
-				"tripleshot" -> Some((("pentashot",100),("quadtank",100),("",100),("",100))),
+				"tank" -> Some((("twin",100),("sniper",100),("",100),("",100))),
+				"twin" -> Some((("tripleshot",100),("quadtank",100),("",100),("",100))),
+				"tripleshot" -> Some((("triplet",100),("pentashot",100),("spreadshot",100),("",100))),
 				"quadtank" -> Some((("octotank",100),("",100),("",100),("",100))),
 				"sniper" -> Some((("stalker",50),("",100),("",100),("",100))),
-				"pentashot" -> Some((("spreadshot",50),("",100),("",100),("",100))),
+				"pentashot" -> None,
 				"octotank" -> None,
 				"spreadshot" -> None,
 				"stalker" -> None,
